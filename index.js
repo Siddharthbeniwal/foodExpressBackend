b4168528f235ba5c009fd7aab461c6d3cb9a5095
Siddharth Beniwal <siddharthben16@gmail.com>
2024-03-06 01:04:03 +0530

    single origin allowed fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigins)
+    res.setHeader("Access-Control-Allow-Origin", origin)
   }
   res.header(
     "Access-Control-Allow-Headers",
